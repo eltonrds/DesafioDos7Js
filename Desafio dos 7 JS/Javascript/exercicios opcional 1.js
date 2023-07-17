b5a6ad57a8a27f121e57d4c34bc7e901a34923c5
Elton Rodrigues de Souza <120459876+eltonrds@users.noreply.github.com>
2023-07-17 20:42:16 -0300

    Add files via upload

---
+++ Desafio dos 7 JS/Javascript/exercicios opcional 1.js
@@ -0,0 +1,27 @@
+// Se você quiser se aprofundar no assunto de hoje, eu tenho mais um exercício para você.
+
+// Mas ele é 100% opcional.
+
+// Você vai complementar o código para que, depois de exibir a mensagem anterior, o programa pergunte:
+
+// Você gosta de estudar [linguagem]? Responda com o número 1 para SIM ou 2 para NÃO.
+
+// E aí, dependendo da resposta, ele deve mostrar uma das seguintes mensagens:
+
+// 1 > Muito bom! Continue estudando e você terá muito sucesso.
+// 2 > Ahh que pena... Já tentou aprender outras linguagens?
+
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+
+alert(msg);
+
+const gosta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 - SIM ou 2 - NÃO`);
+if (gosta == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+}
+if (gosta == 2){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}
---
+++ Desafio dos 7 JS/Javascript/exercicos opcional 2.js
@@ -0,0 +1,38 @@
+// Você já viu estruturas condicionais em Javascript anteriormente, mas vou recapitular. O if é usado para verificar se uma determinada condição é verdadeira.
+
+// Exemplo:
+
+// if (cidade === “Roma”){
+//     // mostre a foto do “Coliseu”
+// }
+
+// Além disso, ele também pode ser usado com um ou vários else if, que irá saber que a condição anterior era falsa e vai verificar se a atual é verdadeira.
+
+// Por fim, existe o else sozinho, sem nenhuma condição, e o código dentro dele será executado sempre que todas as condições encadeadas anteriormente forem falsas.
+
+// if (cidade === “Roma”){
+//     // mostre a foto do “Coliseu”
+// }
+// else if (cidade === “Paris”){
+//     // mostre a foto do “Torre Eiffel”
+// }
+// else {
+//     // dê a resposta “Você não digitou nenhuma cidade válida”
+// }
+
+// Além disso, para a parte 4, você precisará de uma estrutura de repetição (loop) como o while. Para usá-lo, é bem fácil:
+
+// let idade = 0;
+
+// while (idade < 8) {
+//     // algum comando para imprimir a idade
+//     idade = idade + 1;
+// }
+
+// Esse código começará com a idade em zero e, ao entrar no while, esse valor será impresso e, logo depois, incrementado em 1.
+
+// Ou seja, depois da primeira vez que ele passar, o valor da idade será igual a 1, que é menor que 8, e por isso, a condição do while terá um resultado verdadeiro e ele continuará a ser executado.
+
+// Ele só vai parar quando o valor da variável idade chegar a 8, que não é menor que 8, e por isso a condição do while terá um resultado falso.
+
+// Você também pode aprender mais sobre o while nesse site.
